bb9df48a31eb6c4b9befcb99e5906d087d839151
ethansure <jianxupan@foxmail.com>
2015-04-17 00:28:58 +0800

    update

---
+++ public/angular/manage/nav/NavController.js
@@ -0,0 +1,9 @@
+define(['../../app'],function(app){
+	return app.controller('NavController', ['$scope','$rootScope', function($scope,$rootScope){
+		
+		// $scope.stepActive = [true,false,false];
+		
+
+
+	}]);
+});
